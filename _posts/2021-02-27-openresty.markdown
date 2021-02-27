a9fdeb4f43f97db5a2fe972f8026716177e9f061
rayluoluo <canweiluo@163.com>
2021-02-28 00:45:43 +0800

    openresty

---
+++ _posts/2021-02-27-openresty.markdown
@@ -0,0 +1,12 @@
+---
+layout: post
+title:  "openresty"
+category: "nginx"
+tags: "openresty nginx"
+---
+
+# 学习链接
+
+- [OpenResty 最佳实践](https://moonbingbing.gitbooks.io/openresty-best-practices/content/index.html)
+
+- [深入理解Nginx：模块开发与架构解析](https://www.nli.cn/read/ngix-mob/part0002.html)
--- newpost.sh
+++ newpost.sh
@@ -15,9 +15,9 @@ day=`date '+%Y-%m-%d'`
 filename=$day'-'$1'.markdown'
 #echo $filename
 
-[ -e ./_drafts/$filename ] && read -p "$filename already exists. remove[y/Y] or not[n/N]?" y_or_n && [ "${y_or_n}" != "y" ] && [ "${y_or_n}" != "Y" ] && echo "DO NOT create a new draft file. EXIT." && exit 1
+[ -e ./_drafts/"$filename" ] && read -p "$filename already exists. remove[y/Y] or not[n/N]?" y_or_n && [ "${y_or_n}" != "y" ] && [ "${y_or_n}" != "Y" ] && echo "DO NOT create a new draft file. EXIT." && exit 1
 
-cat <<EOF >./_drafts/$filename
+cat <<EOF >./_drafts/"$filename"
 ---
 layout: post
 title:  "$title"
@@ -27,4 +27,4 @@ tags: "$tags"
 EOF
 
 cd ./_drafts
-readlink -f $filename
+readlink -f "$filename"
